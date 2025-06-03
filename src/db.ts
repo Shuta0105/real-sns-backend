4815b55ef436e9cf7796a5992bc8c2d69a59aac3
Shuta0105 <318121aa@gmail.com>
2025-06-03 16:40:40 +0900

    mod

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,13 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 dotenv.config();
-
 
 export const db = mysql.createPool({
   host: process.env.MYSQL_PUBLIC_URL,
-  user: 'root',
+  user: process.env.MYSQLUSER,
   password: process.env.MYSQL_ROOT_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  port: Number(process.env.MYSQLPORT),
   waitForConnections: true,
   connectionLimit: 10,
 });
